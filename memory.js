44b4ab99b3a8a9a75d1a883bb97b663756bfc501
alexandrernascimento <67931718+alexandrernascimento@users.noreply.github.com>
2026-03-11 23:38:05 -0300

    Refactor memory functions and variable names

--- memory.js
+++ memory.js
@@ -1,23 +1,33 @@
 const sessions = {}
 
-export function getMemory(session){
+export function getMemory(session) {
 
-  if(!sessions[session]) sessions[session] = []
+ if (!sessions[session]) {
 
-  return sessions[session]
+  sessions[session] = []
+
+ }
+
+ return sessions[session]
 
 }
 
-export function saveMemory(session,question,answer){
+export function saveMemory(session, user, assistant) {
 
-  sessions[session].push({
-    role:"user",
-    content:question
-  })
+ sessions[session].push({
+  role: "user",
+  content: user
+ })
 
-  sessions[session].push({
-    role:"assistant",
-    content:answer
-  })
+ sessions[session].push({
+  role: "assistant",
+  content: assistant
+ })
+
+ if (sessions[session].length > 10) {
+
+  sessions[session].shift()
+
+ }
 
 }
