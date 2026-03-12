e81d08d78b00c7f42dca19fa4a12590b17231680
alexandrernascimento <67931718+alexandrernascimento@users.noreply.github.com>
2026-03-11 23:36:59 -0300

    Update import path for runAgent function

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import { runAgent } from "./ai/agent.js"
+import { runAgent } from "./agent.js"
 
 dotenv.config()
 
